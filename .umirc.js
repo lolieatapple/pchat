a747e2556255491d52c98217fa73fae4d32259c6
lolieatapple <lolieatapple@gmail.com>
2020-07-01 23:13:55 +0800

    first add

---
+++ .umirc.js
@@ -0,0 +1,36 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        {
+          path: '/chat',
+          component: './chat',
+        },
+        {
+          path: '/',
+          component: '../pages/index',
+        },
+      ],
+    },
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: false,
+        dynamicImport: false,
+        title: 'pchat',
+        dll: false,
+        routes: {
+          exclude: [/components\//],
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/chat.js
@@ -0,0 +1,10 @@
+
+import styles from './chat.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h1>Page chat</h1>
+    </div>
+  );
+}
