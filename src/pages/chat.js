fa5c674e49c561f0619a433d59fca2260a64795d
lolieatapple <lolieatapple@gmail.com>
2020-07-02 23:42:53 +0800

    update ui

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -11,10 +11,26 @@ class Chat extends Component {
           <label>Members: {1}</label>
         </div>
         <div className={styles.history}></div>
-        <input className={styles.input} placeholder={"Input your message here."} />
+        <textarea className={styles.input} placeholder={"Input your message here."} />
         <br/>
-        <select/>
-        <button>Send</button>
+        <div className={styles.sendArea}>
+          <select className={styles.select}>
+            <option value="Mouse">Mouse</option>
+            <option value="Cow">Cow</option>
+            <option value="Tiger">Tiger</option>
+            <option value="Rabbit">Rabbit</option>
+            <option value="Dragon">Dragon</option>
+            <option value="Snake">Snake</option>
+            <option value="Horse">Horse</option>
+            <option value="Sheep">Sheep</option>
+            <option value="Monkey">Monkey</option>
+            <option value="Chicken">Chicken</option>
+            <option value="Dog">Dog</option>
+            <option value="Pig">Pig</option>
+            <option value="Cat">Cat</option>
+          </select>
+          <button className={styles.sendButton}>Send</button>
+        </div>
       </div>
     );
   }
